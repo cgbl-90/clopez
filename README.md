b46131efc05fa632a66c4c8b0edbd0ed29bd21ab
Carmen Lopez <carmenbaez2000@gmail.com>
2024-07-28 13:06:43 +0200

    updates - carousel and content

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# NEW PERSONAL WEBSITE DESIGN
+# MY PERSONAL WEBSITE DESIGN
+
+This website was designed using Javascript, HTML, Saas CSS and Markdown
+
 
 ### About
 
-New portfolio website designed by [Anna Sushok](https://www.linkedin.com/in/anna-sushok/)
+- Version 2 designed by [Carmen L.](https://www.linkedin.com/in/cgbl0/)
+
+- Website version 1 was designed by [Anna Sushok](https://www.linkedin.com/in/anna-sushok/)
+![Home](images/website1.png)![About me](images/website2.png)
+
--- script.js
+++ script.js
@@ -1,159 +1,25 @@
-let btns = document.querySelectorAll("button");
-let articles = document.querySelectorAll("article");
-let mainPage = document.getElementById("mainPage");
-let projectPage = document.getElementById("projectPage");
-let aboutPage = document.getElementById("aboutPage");
-let moreLessClients = document.getElementById("moreLessClients");
-moreLessClients.addEventListener("click", showHideContent);
-let showHideClients = false;
-let revPosition = 0;
-let burgerMenu = document.getElementsByClassName("hideDropdown");
-let burgerOpen = document.getElementsByClassName("openBurger");
-let burgerClose = document.getElementsByClassName("closeBurger");
+document.addEventListener("DOMContentLoaded", function () {
+  const carousel = document.getElementById("carousel");
+  const prevButton = document.getElementById("prev");
+  const nextButton = document.getElementById("next");
+  const carouselWidth =
+    document.getElementById("carousel-container").offsetWidth;
+  let scrollPosition = 0;
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", function (e) {
-    let styles = e.currentTarget.classList;
-    // HIDE & SHOW PAGES
-    if (styles.contains("mainPage")) {
-      // show
-      mainPage.classList.add("enabled");
-      mainPage.classList.remove("disabled");
-      // hide
-      projectPage.classList.remove("enabled");
-      aboutPage.classList.remove("enabled");
-      projectPage.classList.add("disabled");
-      aboutPage.classList.add("disabled");
-      hideBtns();
-    }
-    if (styles.contains("projectPage")) {
-      // show
-      projectPage.classList.add("enabled");
-      projectPage.classList.remove("disabled");
-      // hide
-      mainPage.classList.remove("enabled");
-      mainPage.classList.add("disabled");
-      aboutPage.classList.remove("enabled");
-      aboutPage.classList.add("disabled");
-      hideBtns();
-      //
-      for (let i = 0; i < btns.length; i++)
-        btns[i].classList.remove("underlined");
-      btn.classList.add("underlined");
-    }
-    if (styles.contains("aboutPage")) {
-      // show
-      aboutPage.classList.add("enabled");
-      aboutPage.classList.remove("disabled");
-      // hide
-      mainPage.classList.remove("enabled");
-      mainPage.classList.add("disabled");
-      projectPage.classList.remove("enabled");
-      projectPage.classList.add("disabled");
-      hideBtns();
-      //
-      for (let i = 0; i < btns.length; i++)
-        btns[i].classList.remove("underlined");
-      btn.classList.add("underlined");
-    }
-
-    // CHANGE REVIEW
-
-    if (styles.contains("goNext")) {
-      articles.forEach((art) => {
-        if (art.classList.contains("enabled"))
-          revPosition = Number(art.id.slice(-1));
-      });
-      if (revPosition === articles.length) revPosition = 1;
-      else revPosition = revPosition + 1;
-
-      articles.forEach((art) => {
-        if (art.id.slice(-1) == revPosition) {
-          art.classList.add("enabled");
-          art.classList.remove("disabled");
-        } else {
-          art.classList.add("disabled");
-          art.classList.remove("enabled");
-        }
-      });
-    }
-
-    if (styles.contains("goPrev")) {
-      articles.forEach((art) => {
-        if (art.classList.contains("enabled"))
-          revPosition = Number(art.id.slice(-1));
-      });
-      if (revPosition === 1) revPosition = articles.length;
-      else revPosition = revPosition - 1;
-
-      articles.forEach((art) => {
-        if (art.id.slice(-1) == revPosition) {
-          art.classList.add("enabled");
-          art.classList.remove("disabled");
-        } else {
-          art.classList.add("disabled");
-          art.classList.remove("enabled");
-        }
-      });
-    }
-
-    // BURGER MENU
-
-    if (styles.contains("openBurger")) {
-      burgerMenu[0].style.display = "block";
-      burgerClose[0].style.display = "block";
-      burgerOpen[0].style.display = "none";
-    }
-
-    if (styles.contains("closeBurger")) hideBtns();
-
-    function hideBtns() {
-      burgerMenu[0].style.display = "none";
-      burgerClose[0].style.display = "none";
-      burgerOpen[0].style.display = "block";
+  prevButton.addEventListener("click", () => {
+    if (scrollPosition > 0) {
+      scrollPosition -= carouselWidth;
+      if (scrollPosition < 0) scrollPosition = 0;
+      carousel.style.transform = `translateX(-${scrollPosition}px)`;
     }
   });
-});
 
-// HIDE & SHOW CLIENTS EXTENDED LIST
-
-function showHideContent(e) {
-  e.preventDefault();
-
-  if (!showHideClients) {
-    moreLessClients.insertAdjacentHTML(
-      "beforebegin",
-      `<li class="remove">Harvard</li>`
-    );
-    moreLessClients.insertAdjacentHTML(
-      "beforebegin",
-      `<li class="remove">Typeform</li>`
-    );
-    moreLessClients.insertAdjacentHTML(
-      "beforebegin",
-      `<li class="remove">Workato</li>`
-    );
-    moreLessClients.insertAdjacentHTML(
-      "beforebegin",
-      `<li class="remove">Running Remote</li>`
-    );
-    moreLessClients.innerHTML = `<button class="centered">↑</button>`;
-    showHideClients = true;
-  } else {
-    let Lis = document.querySelectorAll("li");
-    Lis.forEach((el) => {
-      if (el.classList.contains("remove")) el.remove();
-    });
-    moreLessClients.innerHTML = `<li>& More </li>`;
-    showHideClients = false;
-  }
-}
-
-// MEDIA QUERY
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 560) {
-    burgerMenu[0].style.display = "none";
-    burgerClose[0].style.display = "none";
-    burgerOpen[0].style.display = "block";
-  }
+  nextButton.addEventListener("click", () => {
+    if (scrollPosition < carousel.scrollWidth - carouselWidth) {
+      scrollPosition += carouselWidth;
+      if (scrollPosition > carousel.scrollWidth - carouselWidth)
+        scrollPosition = carousel.scrollWidth - carouselWidth;
+      carousel.style.transform = `translateX(-${scrollPosition}px)`;
+    }
+  });
 });

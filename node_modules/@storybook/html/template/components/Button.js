f68ee406d05d670485e0cb5f19aeb749f63a19c3
carmen <58992828+cgbl-90@users.noreply.github.com>
2025-09-28 18:26:08 +0200

    Delete node_modules directory

--- node_modules/@storybook/html/template/components/Button.js
+++
@@ -1,8 +0,0 @@
-export const Button = (args) => {
-  const button = document.createElement('button');
-
-  button.innerHTML = args.label;
-  button.addEventListener('click', args.onClick);
-
-  return button;
-};
